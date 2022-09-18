c23612718ea7d799b1740fb043a4b4386469cde8
leonardoCorti <leonardo.filippo@ymail.com>
2022-09-19 00:53:48 +0200

    working correctly, write type and content

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from lib2to3.pgen2 import token
 import re
 
 def clipTest(clip):
@@ -7,25 +6,18 @@ def clipTest(clip):
 
     return 0
 
-
 clippingsFile = open("input\My Clippings.txt", "r", encoding="UTF-8")
 allClippings = clippingsFile.read()
 clippingsFile.close()
 listOfClippings = allClippings.split("==========\n")
-
 tokens={ #italian for now
     "bookmark": "Il tuo segnalibro",
     "highlightPage": "La tua evidenziazione",
     "note": "La tua nota",
     "date": "Aggiunto in data"
 }
-
 typesOfClip = {"bookmark", "highlightPage", "note"}
 
-
-print(len(listOfClippings))
-
-i= 0
 for clipping in listOfClippings:
     clippingDict = {}
     linesOfClipping = clipping.splitlines()
@@ -33,12 +25,19 @@ for clipping in listOfClippings:
     # linesOfClipping[1] #type of highlight, page/position and date
     # linesOfClipping[2] #nothing
     # linesOfClipping[3] #highlight or note
-    clippingDict["name"]= re.findall("^.*(?=\()",linesOfClipping[0])[0]
-    clippingDict["author"]= linesOfClipping[0].replace(clippingDict["name"]," ").replace("(","").replace(")","")
+    if(len(linesOfClipping)<4): break
+    if(re.match("^.*(?=\()",linesOfClipping[0])):
+        clippingDict["name"]= re.findall("^.*(?=\()",linesOfClipping[0])[0]
+        clippingDict["author"]= linesOfClipping[0].replace(clippingDict["name"]," ").replace("(","").replace(")","")
+    else: clippingDict["name"]= linesOfClipping[0]
     for type in typesOfClip:
         if(re.search(tokens[type],linesOfClipping[1])): clippingDict["type"]=type
     clippingDict["highlight"] = linesOfClipping[3]
+    outputFileName= "output\\"+clippingDict["name"].strip().replace("/","")+".md"
+    # print(outputFileName)
+    output = open(outputFileName, "a", encoding="UTF-8")
+    
+    if(clippingDict["type"] != "bookmark"):output.write(f"{clippingDict['type']}:\n{clippingDict['highlight']}\n\n")
 
-    clipTest(clippingDict)
-    i +=1
-    if ( i == 50): break
+    output.close()
+    #clipTest(clippingDict)
