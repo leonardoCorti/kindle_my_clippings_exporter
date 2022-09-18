d838da59e595c5bfc1a6764c29a1003df0bf8448
leonardoCorti <leonardo.filippo@ymail.com>
2022-09-18 23:20:55 +0200

    added token dictonary in ita and other stuff

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+
+def clipTest(clip):
+    for key in clip.keys():
+        print(f"{key}: {clip[key]}")
+
+    return 0
+
+
 clippingsFile = open("input\My Clippings.txt", "r", encoding="UTF-8")
 allClippings = clippingsFile.read()
 clippingsFile.close()
-
 listOfClippings = allClippings.split("==========")
+
+tokensLanguageDependeant={ #italian for now
+    "bookmark" : "Il tuo segnalibro",
+    "date" : "Aggiunto in data"
+}
+
 
 print(len(listOfClippings))
 
 i= 0
-for line in listOfClippings:
+for clipping in listOfClippings:
+    clippingDict = {}
+    #print(len(clipping.splitlines()))
+    #print(clipping)
+    for line in clipping.splitlines():
+        print(f"a\n {line.strip()}\nb")
+    
     i +=1
-    print(line.strip())
-    print("\n\n")
-    if ( i == 10): break
+    if ( i == 4): break
