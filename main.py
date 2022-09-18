a3351937c88f75a2aa1bff55b0b8146c4dc42fc2
leonardoCorti <leonardo.filippo@ymail.com>
2022-09-18 23:58:14 +0200

    author and name

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 
 def clipTest(clip):
     for key in clip.keys():
@@ -9,11 +10,14 @@ def clipTest(clip):
 clippingsFile = open("input\My Clippings.txt", "r", encoding="UTF-8")
 allClippings = clippingsFile.read()
 clippingsFile.close()
-listOfClippings = allClippings.split("==========")
+listOfClippings = allClippings.split("==========\n")
 
 tokensLanguageDependeant={ #italian for now
-    "bookmark" : "Il tuo segnalibro",
-    "date" : "Aggiunto in data"
+    "bookmark": "Il tuo segnalibro",
+    "highlightPage": "La tua evidenziazione a pagina",
+    "note": "La tua nota a",
+    "highlightPosition": "Il tuo segnalibro alla posizione",
+    "date": "Aggiunto in data"
 }
 
 
@@ -22,10 +26,17 @@ print(len(listOfClippings))
 i= 0
 for clipping in listOfClippings:
     clippingDict = {}
-    #print(len(clipping.splitlines()))
-    #print(clipping)
-    for line in clipping.splitlines():
-        print(f"a\n {line.strip()}\nb")
-    
+    linesOfClipping = clipping.splitlines()
+    # linesOfClipping[0] #name and author
+    # linesOfClipping[1] #type of highlight, page/position and date
+    # linesOfClipping[2] #nothing
+    # linesOfClipping[3] #highlight or note
+    clippingDict["name"]= re.findall("^.*(?=\()",linesOfClipping[0])[0]
+    linemodified= linesOfClipping[0].replace(clippingDict["name"]," ")
+    linemodified= linemodified.replace("(","")
+    linemodified= linemodified.replace(")","")
+    clippingDict["author"]= linesOfClipping[0].replace(clippingDict["name"]," ").replace("(","").replace(")","")
+    clipTest(clippingDict)
+
     i +=1
     if ( i == 4): break
